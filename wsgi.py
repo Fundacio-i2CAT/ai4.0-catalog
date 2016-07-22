116714fbc4a4c7a2a6fd80308a1720aa2795577d
Oscar Rambla <oscar_rambla@i2cat.net>
2016-07-22 14:16:11 +0200

    Threading app and resp time log

--- wsgi.py
+++ wsgi.py
@@ -1,12 +1,26 @@
+import time
 from anella.flask_app import create_app
+from anella import output
 
 app = create_app()
+
+start = stop = 0
+
+@app.before_request
+def before_request():
+  global start
+  start = time.time()
+  output.OUTPUT.info("Req. start time %s " % (start))
 
 @app.after_request
 def after_request(response):
   """
   Suport for CORS at Flask app
   """
+  global stop
+  stop = time.time()
+  output.OUTPUT.info("Response time %s " % (stop-start))
+
   response.headers.add('Access-Control-Allow-Origin', '*')
   response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
   response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
@@ -17,4 +31,4 @@ def bootstrap_app():
   return app
 
 if __name__ == "__main__":
-    app.run(host=app.config['HOST'], port=app.config['PORT'], processes=3 )
+    app.run(host=app.config['HOST'], port=app.config['PORT'], threaded=True )
