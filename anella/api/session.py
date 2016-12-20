e9316cb6e2b0336312c5945ca0362bd3c73e09c7
david_vicens <Erni2016>
2016-12-20 14:37:43 +0100

    Merge with master

--- anella/api/session.py
+++ anella/api/session.py
@@ -1,35 +1,17 @@
 from anella.common import get_json, get_session
-from anella.model.user import User
 
 from anella.api.utils import ItemRes, respond_json, error_api
 from anella.api.user import UserRes
+from anella.auth import Authenticator
+
 
 class SessionRes(ItemRes):
+    def __init__(self):
+        self.auth = Authenticator()
 
     def post(self):
-        try:
-            data = get_json()
-            if 'user_name' in data:
-                user = User.objects.get(user_name=data['user_name'])
-            elif 'email' in data:
-                user = User.objects.get(email=data['email'])
-            else:
-                error_api("Missing email,user_name.", status=400)
-    
-            if user.has_password(data['password']):
-                get_session()['user']=unicode(user.pk)
-                # item = get_db()['users'].find_one({'_id':user.pk})
-                # item = UserRes()._item_to_json(item) 
-                session_id = get_session().sid
-                response = dict( status='ok', user_id=unicode(user.pk), session_id=session_id,
-                                 msg="Welcomed '%s'." % user.user_name )
-                return respond_json( response, status=200)
-    
-            response = dict( status='fail', msg="Wrong password." )
-            return respond_json( response, status=400)
-        except Exception, e:
-            response = dict( status='fail', msg=unicode(e) )
-            return respond_json( response, status=400)
+        item = get_json()
+        return respond_json('', status=self.auth.user_login(item['user_name'], item['password']))
     
     def delete(self):
         try:
--- anella/auth.py
+++ anella/auth.py
@@ -11,33 +11,45 @@ from requests import get, put, post, delete, Session
 
 from anella.utils import load_config
 from anella.common import *
+from itsdangerous import (TimedJSONWebSignatureSerializer
+                          as Serializer, BadSignature, SignatureExpired)
 
 LOGGER = logging.getLogger('stdout')
 
 class Authenticator(object):
 
-   def __init__(self):
-       self.root_path='http://%s:%s/LmpApi/' % (get_cfg('auth__host'), get_cfg('auth__port'))
+    def __init__(self):
+       self.root_path='http://%s:%s/LmpApiI2cat/' % (get_cfg('auth__host'), get_cfg('auth__port'))
        self.session = Session()
        # For debugging purposes
        self.r_data = None
        self.path = None
 
-   def user_find(self, user):
-       self.path = self.root_path+'people/search/findFirstByEmail?email='+quote_plus(user.email)
-       self.req = self.session.get(self.path)
-       if self.req.status_code==200:
-           return json.loads(self.req.text)
+    def user_login(self, email, password):
+        path = self.root_path + 'loginWithPassword?email=' + email + '&password=' + password
+        req = self.session.get(path)
+        token = self.verify_auth_token('eyJhbGciOiJIUzI1NiIsImV4cCI6MTQ4MTEzMDE0MywiaWF0IjoxNDgxMTI5ODQzfQ.eyJpZCI6IjI1In0.m8wAxNP36N39WFR65at7w4qXVsJIr8-kyGXAgmvjiYI')
+        #token = self.generate_auth_token()
+        print token
+        return req.status_code
+
+        
+
+    def user_find(self, user):
+        self.path = self.root_path+'people/search/findFirstByEmail?email='+quote_plus(user.email)
+        self.req = self.session.get(self.path)
+        if self.req.status_code==200:
+            return json.loads(self.req.text)
 
 
-   def user_get(self, id):
+    def user_get(self, id):
        self.path = self.root_path+'people/'+str(id)
        self.req = self.session.get(self.path)
        if self.req.status_code==200:
            return json.loads(self.req.text)
 
 
-   def user_auth(self, user, password):
+    def user_auth(self, user, password):
        if user.auth_id:
            auth = self.user_get(user.auth_id)
        else:
@@ -47,38 +59,18 @@ class Authenticator(object):
        else:
            LOGGER.debug("User '%s' not found." % user.user_name)
 
-
-   def user_create(self, user, password):
-       # import pdb;pdb.set_trace()
-       auth = self.user_find(user)
-       if auth:
-           user.auth_id = auth['id']
-           user.save()
-           return
-
-       self.path = self.root_path+'people'
-       self.r_data = dict(name=user.first_name, surname=user.last_name, 
-                     email=user.email, password=password,
-                     identifier=user.nif, 
-                     phone=user.phone_number )
+    def user_create(self, item):
+        req = self.session.post(self.root_path + 'people', json=item)
+        data = json.loads(req.text)
+        return {'code': req.status_code, 'message': data['cause']['cause']['message']}
    
-       self.req = self.session.post(self.path, json=self.r_data)
-       if self.req.status_code==201:
-           auth_id= json.loads(self.req.text)['id']
-           user.auth_id = auth['id']
-           user.save()
-           return auth_id
-           
-       return self.req.status_code==201
-
-   
-   def entity_find(self, partner):
+    def entity_find(self, partner):
        self.path = self.root_path+'entities/search/findFirstByEmail?email='+quote_plus(partner.contact.email)
        self.req = self.session.get(self.path)
        return self.req.status_code == 200
 
 
-   def entity_create(self, partner):
+    def entity_create(self, partner):
        if self.entity_find(partner):
            return
 
@@ -87,5 +79,23 @@ class Authenticator(object):
    
        self.req = self.session.post(self.path, json=self.r_data)
        return self.req.status_code==201
+
+    def generate_auth_token(self, expiration=300):
+        s = Serializer('secret-key', expires_in=expiration)
+        return s.dumps({'id': '25'})
+
+    @staticmethod
+    def verify_auth_token(token):
+        s = Serializer('secret-key')
+        try:
+            data = s.loads(token)
+        except SignatureExpired:
+            print 'entar aqui'
+            return None  # valid token, but expired
+        except BadSignature:
+            print 'aquiiiii'
+            return None  # invalid token
+        print data
+        return "ok"
 
 
