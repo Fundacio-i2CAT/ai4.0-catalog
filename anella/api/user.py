e7353bf46cc350bb30ac0976fb5e45f6e9c05dea
david_vicens <Erni2016>
2016-12-22 12:18:31 +0100

    API Eurecat activar / desactivar (login con cookie)

--- anella/api/user.py
+++ anella/api/user.py
@@ -3,8 +3,30 @@
 from anella.common import get_db
 from anella.model.user import User
 
-from anella.api.utils import ColRes, ItemRes, item_to_json
+from anella.api.utils import ColRes, ItemRes, item_to_json, respond_json, get_json
 from anella import configuration as _cfg
+from requests import Session
+import json
+
+class UsersCrudRes(ColRes):
+    def __init__(self):
+        self.root_path = '%s%s' % (_cfg.auth__eurecat, 'people')
+        self.session = Session()
+
+    def get(self):
+        req = self.session.get(self.root_path)
+        return get_response(req)
+
+class UserCrudRes(ColRes):
+    def __init__(self):
+        self.root_path = '%s%s' % (_cfg.auth__eurecat, 'people/')
+        self.session = Session()
+
+    def patch(self, id):
+        data = get_json()
+        path = self.root_path + id
+        req = self.session.patch(path, headers={'Content-Type': 'application/json'}, json=data)
+        return get_response(req)
 
 class UsersRes(ColRes):
     collection = 'users'
@@ -40,3 +62,9 @@ def partner_to_json(item):
 
     return item
 
+def get_response(req):
+    if req.status_code == 200:
+        data = respond_json(json.loads(req.text), status=req.status_code)
+    else:
+        data = respond_json(dict(msg='nok'), status=req.status_code)
+    return data
