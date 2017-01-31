de7d391884abf054b55d44283d298532a32f7557
david_vicens <Erni2016>
2017-01-31 15:27:28 +0100

    Create message_error

--- anella/api/project.py
+++ anella/api/project.py
@@ -199,12 +199,12 @@ class ProjectStateRes(ProjectRes):
                     json_load = json.loads(resp.text)
                     if 'code' in json_load:
                         resp_text = json_load['code']
-                    return create_message_error(resp.status_code, resp_text, get_status('FAILED'))
+                    return create_message_error(resp.status_code, resp_text, '')
 
         error = self._get_state(services)
         if error['status_code'] not in (200,201):
             return create_message_error(self.orch.req.status_code,
-                                        json.loads(self.orch.req.text)['code'], get_status('FAILED'))
+                                        json.loads(self.orch.req.text)['code'], '')
 
     def exists_image(self, service):
         context = dict(pop_id=1)
@@ -260,7 +260,7 @@ class ProjectStateRes(ProjectRes):
                     #break
 
         if (project_status is None) or (data is None):
-            return create_message_error(404, _cfg.errors__orchestrator_state, get_status('FAILED'))
+            return create_message_error(404, _cfg.errors__orchestrator_state,'')
         else:
             return dict(status_code=self.orch.req.status_code, status=project_status,
                         state=STATES[project_status], runtime_params=data['runtime_params'])
