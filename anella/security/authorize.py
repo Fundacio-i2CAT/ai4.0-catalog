b5e04ef9317e0b75af4bfbf693f05a570ca4345b
david_vicens <Erni2016>
2017-03-07 17:59:31 +0100

    Mejora en proceso de errores

--- anella/security/authorize.py
+++ anella/security/authorize.py
@@ -9,17 +9,15 @@ def authorizate(fn):
     @wraps(fn)
     def decorated_view(*args, **kwargs):
         jwt_token = get_token()
-        print get_path()
-        print
         if jwt_token:
             try:
                 jwt.decode(jwt_token, 'secret',
                            algorithms=['HS256'])
             except (jwt.DecodeError, jwt.ExpiredSignatureError):
-                return respond_json(create_message_error(403,dict(message='TOKEN_EXPIRED')),
+                return respond_json(create_message_error(403, 'TOKEN_EXPIRED'),
                                     status=403)
         elif get_path() != '/api/session':
-            return respond_json(create_message_error(403,dict(message='TOKEN_EXPIRED')),
+            return respond_json(create_message_error(403, 'TOKEN_EXPIRED'),
                                 status=403)
         return fn(*args, **kwargs)
     return decorated_view
