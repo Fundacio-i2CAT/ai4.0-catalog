e1daf7ef0181c83265c40279f21e3370e109f9c1
alfonso_egio <alfonso.egio@i2cat.net>
2017-03-28 12:44:42 +0200

    Updatear y eliminar ruta del fichero de error

--- anella/api/project.py
+++ anella/api/project.py
@@ -625,12 +625,15 @@ class ProjectOrchCallbackRes(ProjectsRes):
                 instance = get_db(_cfg.database__database_name)['instances'].find_one({'instance_id': instance_info['service_instance_id']})
                 sproject = get_db(_cfg.database__database_name)['sprojects'].find_one({'_id': ObjectId(instance['sproject'])})
                 service = get_db(_cfg.database__database_name)['services'].find_one({'_id': ObjectId(sproject['service'])})
+                get_db(_cfg.database__database_name)['sprojects'].update({'_id': ObjectId(instance['sproject'])},
+                                                                         {'$set': {'status': 5}},
+                                                                         upsert=False)
                 image_path = '{0}.img'.format(str(service['context']['vm_image']))
                 file_to_remove = '{0}{1}'.format(_cfg.repository__path,
                                                   image_path)
                 os.remove(file_to_remove)
             except:
-                respond_json({'message': 'Error removing {0}'.format(file_to_remove)}, status=500)
+                respond_json({'message': 'Error removing'}, status=500)
         return respond_json(instance_info, status=200)
 
 
