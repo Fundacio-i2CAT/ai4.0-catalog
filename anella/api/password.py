33a958af1151313adcf548138ce431a3a11f1cdb
david_vicens <Erni2016>
2017-05-02 15:43:36 +0200

    Post para recuperar contraseña

--- anella/api/password.py
+++ anella/api/password.py
@@ -1,4 +1,4 @@
-from anella.api.utils import ItemRes, create_response, create_message_error, get_json
+from anella.api.utils import ColRes, ItemRes, create_response, create_message_error, get_json
 from anella.api.service_manager_mailer import ServiceManagerMailer
 import random, string
 from anella.api.keystone import Keystone
@@ -11,6 +11,33 @@ def generate_password():
     rand_str = lambda n: ''.join([random.choice(string.lowercase) for i in xrange(n)])
     return rand_str(15)
 
+
+class PasswordRes(ColRes):
+    def __init__(self):
+        self.smm = ServiceManagerMailer()
+        self.collection = 'users'
+        self.fields = 'user_name,email,keystone_user_id,activated'.split(",")
+        self.keystone = Keystone()
+        self.item = None
+        self.status_code = 204
+        self.u = User()
+
+    def post(self):
+        data = get_json()
+        user = self.u.get_by_field('email', data['email'])
+        if user is None:
+            self.item = create_message_error(404, '')
+            return create_response(404, self.item)
+        new_password = generate_password()
+        response = self.keystone.patch_user(user['keystone_user_id'], new_password)
+        if response.status_code == 200:
+            self.smm.change_password(user['email'], new_password)
+            self.u.update(str(user['_id']), {"password": True})
+        else:
+            self.status_code = response.status_code
+            self.item = create_message_error(self.status_code, json.loads(response.text))
+
+        return create_response(self.status_code, self.item)
 
 class Password(ItemRes):
     def __init__(self):
