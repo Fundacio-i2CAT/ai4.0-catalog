2aeb882ad6493d502a230c07570753a6340beda7
david_vicens <Erni2016>
2017-05-03 15:16:05 +0200

    Control en los campos que se guardan en BBDD

--- anella/model/user.py
+++ anella/model/user.py
@@ -22,6 +22,25 @@ class UserRole(object):
         self.user_name = None
         self.password = None
 
+
+def dictionary_user(data):
+    user = {}
+    if 'user_name' in data: user['user_name'] = data['user_name']
+    if 'activated' in data: user['activated'] = data['activated']
+    if 'name' in data: user['name'] = data['name']
+    if 'surname' in data: user['surname'] = data['surname']
+    if 'email' in data: user['email'] = data['email']
+    if 'company' in data: user['company'] = data['company']
+    if 'address' in data: user['address'] = data['address']
+    if 'phone' in data: user['phone'] = data['phone']
+    if 'position' in data: user['position'] = data['position']
+    if 'legal' in data: user['legal'] = data['legal']
+    if 'identification' in data: user['identification'] = data['identification']
+    if 'deleted' in data: user['deleted'] = data['deleted']
+    if 'password' in data: user['password'] = data['password']
+    return user
+
+
 class User(Document, Base):
     meta = {'allow_inheritance': True, 'collection': 'users'}
 
@@ -54,8 +73,9 @@ class User(Document, Base):
 
     def update(self, id, data):
         return get_db(cfg.database__database_name).get_collection('users'). \
-            update_one({'_id': ObjectId(id)}, {"$set": data},
+            update_one({'_id': ObjectId(id)}, {"$set": dictionary_user(data)},
                        upsert=False)
+
 
 class Provider(User):
     _cls = "User.Provider"
